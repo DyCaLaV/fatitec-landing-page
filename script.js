d691961b2566062ba202ab1b382153fd9b8c62e3
DyCaLaV <jeremyleonpari2006@gmail.com>
2026-07-09 18:11:31 -0500

    add he_menu_hamburguesa add he_responsive add script.js updating index.html

---
+++ script.js
@@ -0,0 +1,11 @@
+// Selecciona el botón de hamburguesa
+const btn = document.querySelector(".menu-toggle");
+
+// Selecciona el menú horizontal
+const menu = document.querySelector(".menu-horizontal");
+
+// Al hacer clic en el botón de hamburguesa
+btn.addEventListener("click", () => {
+  // Alterna (agrega o quita) la clase "active" en el menú
+  menu.classList.toggle("active");
+});
